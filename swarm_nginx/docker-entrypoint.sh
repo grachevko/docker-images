7d65a4ab12c2fc27d8e2e7ac257f302680f29952
Konstantin Grachev <ko@grachev.io>
2016-10-16 11:51:14 +0300

    Redirect error echo to stderr

--- swarm_nginx/docker-entrypoint.sh
+++ swarm_nginx/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ -z $NGINX_BACKEND_HOST ]; then
-    echo env "NGINX_BACKEND_HOST" must be declared
+    >&2 echo env "NGINX_BACKEND_HOST" must be declared
     exit 1
 else
     sed -i 's/%BACKEND_HOST%/'"$NGINX_BACKEND_HOST"'/g' /etc/nginx/nginx.conf
